3972de47c4ec3085a26350d60b4459ce8b97e112
Ritika0919-sudo <pandeyritika686@gmail.com>
2026-02-02 01:40:43 +0530

    Update experiment3.cpp

--- experiment3.cpp
+++ experiment3.cpp
@@ -3,13 +3,12 @@
 #include <algorithm>
 using namespace std;
 
-
 int main() {
     int N;
     cin >> N;
 
-    unordered_map<int, int> firstIndex;
-    firstIndex[0] = -1;
+    unordered_map<int, int> mp;
+    mp[0] = -1;   // base case
 
     int sum = 0, maxLen = 0;
     char ch;
@@ -17,21 +16,26 @@ int main() {
     for (int i = 0; i < N; i++) {
         cin >> ch;
 
-        if (ch == 'P') sum++;
-        else if (ch == 'A') sum--;
-
-        if (firstIndex.count(sum))
-            maxLen = max(maxLen, i - firstIndex[sum]);
+        if (ch == 'P')
+            sum += 1;
         else
-            firstIndex[sum] = i;
+            sum -= 1;
+
+        if (mp.find(sum) != mp.end()) {
+            maxLen = max(maxLen, i - mp[sum]);
+        } else {
+            mp[sum] = i;
+        }
     }
 
     cout << maxLen;
     return 0;
 }
 
+
 /*INPUT :
 6
 P A P A P P
 OUTPUT:
 4*/
+
