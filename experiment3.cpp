5f1f1580117e9c6e58eb9d8bda157c764e5da19c
Ritika0919-sudo <pandeyritika686@gmail.com>
2026-02-02 01:34:23 +0530

    Add files via upload

---
+++ experiment3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+using namespace std;
+
+/*
+int main() {
+    int N;
+    cin >> N;
+
+    unordered_map<int, int> firstIndex;
+    firstIndex[0] = -1;
+
+    int sum = 0, maxLen = 0;
+    char ch;
+
+    for (int i = 0; i < N; i++) {
+        cin >> ch;
+
+        if (ch == 'P') sum++;
+        else if (ch == 'A') sum--;
+
+        if (firstIndex.count(sum))
+            maxLen = max(maxLen, i - firstIndex[sum]);
+        else
+            firstIndex[sum] = i;
+    }
+
+    cout << maxLen;
+    return 0;
+}
+*/
